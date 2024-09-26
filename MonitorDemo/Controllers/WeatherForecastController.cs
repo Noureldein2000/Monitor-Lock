c791a4f87706be1ec4dc59191bf1d7ccf35bee81
Nour <noureldin@tafeel.com>
2024-09-26 10:29:20 +0300

    Update .gitignore to exclude .vs directory

---
+++ MonitorDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,115 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace MonitorDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly object _object = 1;
+
+        //private static readonly Object obj = new Object();
+        private ConcurrentDictionary<string, object> dictionary = new ConcurrentDictionary<string, object>();
+
+        private readonly LockManager _lockManager = new LockManager();
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> GetWeatherForecast()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("GetById")]
+        public bool GetById(int id)
+        {
+            return ProcessNumber(id);
+
+            //Boolean _lockTaken = false;
+            //var obj = dictionary.GetOrAdd(id.ToString(), new object());
+            //Monitor.Enter(obj, ref _lockTaken);
+
+            //lock (obj)
+            //{
+
+            //    for (int i = 0; i < 5; i++)
+            //    {
+            //        Thread.Sleep(1000);
+            //        Console.Write(i + ",");
+            //    }
+            //    _lockTaken = true;
+            //}
+            ////finally
+            ////{
+            ////    if (_lockTaken)
+            ////    {
+            ////        Monitor.Exit(_object);
+            ////    }
+            ////}
+            //Console.WriteLine(_lockTaken.ToString());
+            //return _lockTaken;
+        }
+
+        [HttpGet("GetById2")]
+        public void GetById2(int id)
+        {
+            var obj = dictionary.GetOrAdd(id.ToString(), new object());
+            lock (obj)
+            {
+
+            }
+        }
+
+        private bool ProcessNumber(int number)
+        {
+            if (_lockManager.IsNumberLocked(number))
+            {
+                // Handle the case when the number is locked
+                Console.WriteLine($"Number {number} is currently locked.");
+                return false;
+            }
+
+            // Try to lock the number
+            if (_lockManager.TryLockNumber(number))
+            {
+                try
+                {
+                    // Perform your processing here
+                    Console.WriteLine($"Processing number {number}...");
+                    Thread.Sleep(2000); // Simulating work
+                    return true;
+                }
+                finally
+                {
+                    // Always release the lock
+                    _lockManager.ReleaseLock(number);
+                }
+            }
+            else
+            {
+                // Could not acquire the lock
+                Console.WriteLine($"Could not lock number {number} as it's already in use.");
+                return false;
+            }
+        }
+
+    }
+}
---
+++ MonitorDemo/LockManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+public class LockManager
+{
+    private static readonly ConcurrentDictionary<object, SemaphoreSlim> _locks = new ConcurrentDictionary<object, SemaphoreSlim>();
+    
+    public bool IsNumberLocked(object number)
+    {
+        // Check if the lock exists for the number
+        if (_locks.ContainsKey(number))
+        {
+            return true; // Number is locked
+        }
+        return false; // Number is not locked
+    }
+
+    public bool TryLockNumber(object number)
+    {
+        // Try to add a new lock for the number
+        var semaphore = _locks.GetOrAdd(number, _ => new SemaphoreSlim(1, 1));
+
+        // Try to enter the semaphore without blocking
+        if (semaphore.Wait(0))
+        {
+            // Successfully acquired the lock
+            return true;
+        }
+
+        // Lock already exists; could not acquire it
+        return false;
+    }
+
+    public void ReleaseLock(object number)
+    {
+        if (_locks.TryGetValue(number, out var semaphore))
+        {
+            // Release the semaphore and remove it if it's free
+            semaphore.Release();
+
+            // Remove the lock if no more threads are waiting
+            if (semaphore.CurrentCount == 1) // If no threads are waiting
+            {
+                _locks.TryRemove(number, out _);
+            }
+        }
+    }
+}
